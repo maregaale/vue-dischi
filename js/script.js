2ab550f68b6efb416464f80dbd1767c8a3990614
Alessandro Marega <ale_marega@hotmail.it>
2021-04-08 15:57:21 +0200

    aggiunta carta in maniera dinamica con oggetto di prova

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ const app = new Vue (
   {
     el: "#root",
     data: {
-
+      cards: [
+        {
+          poster: "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+          title: "New Jersey",
+          author: "Bon Jovi",
+          genre: "Rock",
+          year: "1988"
+        },
+      ],
     },
   }
 );
