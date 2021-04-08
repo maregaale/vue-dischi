9f2feecb2295373c6dc0b8839d94764bec5e3cb8
Alessandro Marega <ale_marega@hotmail.it>
2021-04-08 16:37:10 +0200

    aggiunta select dinamica con i generi dei dischi, fix sintassi

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@ const app = new Vue (
   {
     el: "#root",
     data: {
-      cards: [],
+      disks: [],
+      genres: [],
     },
     mounted: function () {
+      // chiamata API
       axios.get("https://flynn.boolean.careers/exercises/api/array/music")
         .then( (reply) => {
-          this.cards = reply.data.response;
+          // aggiunta carte
+          this.disks = reply.data.response;
+
+          // aggiunta generi
+          reply.data.response.forEach((item, i) => {
+            if (!this.genres.includes(item.genre)) {
+              this.genres.push(item.genre);
+            }
+          });
       });
     },
   }
